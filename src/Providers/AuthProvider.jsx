2d6ba41da408ea21b52ae24c8b83842845e73312
Sumaiya Sinja <sumiyasinja@gmail.com>
2024-03-04 14:04:54 +0600

    Context API created

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,1 +1,15 @@
 // <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-600"></div>
+
+import React, { createContext } from 'react';
+// context API
+export AuthContext = createContext(null);
+const AuthProvider = ({children}) => {
+    const authinfo ={
+
+    }
+    return (
+        <AuthContext.Provider value = {authinfo}></AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router as Router } from './Routes/Router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router as Router } from "./Routes/Router";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Providers/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={Router} />
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
