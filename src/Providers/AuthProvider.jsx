02a7d96ec8010b5c51d66622a74c07891bd876de
Sumaiya Sinja <sumiyasinja@gmail.com>
2024-03-04 14:36:19 +0600

    google login added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,15 +1,23 @@
 // <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-600"></div>
 
-import React, { createContext } from 'react';
+import  { createContext } from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from '../Firebase/firebase.config';
+
 // context API
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
-    const authinfo ={
 
+    const GoogleProvider = new GoogleAuthProvider()
+    const LogInWithGoogle = () =>{
+        return signInWithPopup(auth, GoogleProvider)
+    }
+    const authinfo ={
+        LogInWithGoogle,
     }
     return (
-        <AuthContext.Provider value = {authinfo}></AuthContext.Provider>
+        <AuthContext.Provider value = {authinfo}>{children}</AuthContext.Provider>
     );
 };
 
