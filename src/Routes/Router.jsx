4442bf5f60d3c9d281da234c5ac9c708eb66ab42
Sumaiya Sinja <sumiyasinja@gmail.com>
2024-03-06 12:44:53 +0600

    update page added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,35 +1,39 @@
-  // Router.jsx
-  import { createBrowserRouter } from 'react-router-dom';
-  import App from './../App';
-  import Home from './../Pages/Home/Home';
-  import Login from './../Pages/Login/Login';
-  import Registration from './../Pages/Registration/Registration';
-  import ErrorPage from './../Pages/Error/ErrorPage';
-import AssignAssignment from '../Pages/AssignAssignment/AssignAssignment';
-  
-  
-  export const router = createBrowserRouter([
+// Router.jsx
+import { createBrowserRouter } from "react-router-dom";
+import App from "./../App";
+import Home from "./../Pages/Home/Home";
+import Login from "./../Pages/Login/Login";
+import Registration from "./../Pages/Registration/Registration";
+import ErrorPage from "./../Pages/Error/ErrorPage";
+import AssignAssignment from "../Pages/AssignAssignment/AssignAssignment";
+import UpdateAssignment from "../Pages/UpdateAssignment/UpdateAssignment";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
       {
         path: "/",
-        element: <App/>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children: [
-          {
-            path: "/",
-            element: <Home/>,
-          },
-          {
-            path: "/login",
-            element: <Login/>,
-          },
-          {
-            path: "/register",
-            element: <Registration/>,
-          },
-          {
-            path: "/create-assignment",
-            element: <AssignAssignment/>,
-          },
-        ],
+        element: <Home />,
       },
-    ]);
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Registration />,
+      },
+      {
+        path: "/create-assignment",
+        element: <AssignAssignment />,
+      },
+      {
+        path: "/assignmen/update/:id",
+        element: <UpdateAssignment />,
+      },
+    ],
+  },
+]);
